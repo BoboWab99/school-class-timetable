449deb9db3c94527c6ba3397efc4cf91b90cf1be
BoboWab99 <wingabire.arnaud@strathmore.edu>
2023-03-06 00:47:58 +0300

    first working solution

--- calendar.js
+++ calendar.js
@@ -2,23 +2,9 @@
 
 const data = [
    {
-      title: 'Intro to DB',
-      venue: 'Elgon',
-      start: '08:15',
-      duration: 1,
-      day: 1
-   },
-   {
-      title: 'Intro',
-      venue: 'Kindaruma',
-      start: '08:15',
-      duration: 4,
-      day: 1
-   },
-   {
       title: 'Testing',
       venue: 'Suswa',
-      start: '08:15',
+      start: '09:15',
       duration: 3,
       day: 1
    },
@@ -28,6 +14,13 @@ const data = [
       start: '08:15',
       duration: 2,
       day: 1
+   },
+   {
+      title: 'Programming',
+      venue: 'Longonot',
+      start: '08:15',
+      duration: 2,
+      day: 3
    },
    {
       title: 'DSA',
@@ -65,40 +58,155 @@ const data = [
       day: 2
    },
    {
+      title: 'Web dev',
+      venue: 'Elgon',
+      start: '17:30',
+      duration: 3,
+      day: 1
+   },
+   {
       title: 'Calculus',
       venue: 'Elgon',
       start: '08:15',
       duration: 3,
       day: 6
-   }
+   },
+   {
+      title: 'Database Systems',
+      venue: 'Elgon',
+      start: '08:15',
+      duration: 1,
+      day: 1
+   },
+   {
+      title: 'Intro',
+      venue: 'Kindaruma',
+      start: '08:15',
+      duration: 4,
+      day: 1
+   },
+   {
+      title: 'Intro',
+      venue: 'Kindaruma',
+      start: '13:15',
+      duration: 2,
+      day: 3
+   },
+   {
+      title: 'Intro',
+      venue: 'Kindaruma',
+      start: '12:15',
+      duration: 2,
+      day: 2
+   },
+   {
+      title: 'Testing',
+      venue: 'Kindaruma',
+      start: '11:15',
+      duration: 4,
+      day: 2
+   },
+   {
+      title: 'Calculus',
+      venue: 'Elgon',
+      start: '08:15',
+      duration: 3,
+      day: 2
+   },
 ]
 
-data.sort((a, b) => b.duration - a.duration)
+// arrange data by day
+let dayData = {}
 
-setTimeout(() => {
-   data.forEach(course => {
-      const rowId = `row${course.day}`
-      const startCellId = `cell${course.day}${course.start.slice(0, 2) + course.start.slice(3)}`
+const arrangeData = (dayBookings, day = 1) => {
+   let sorted = []
+   let counter = 0
+   sorted[counter] = []
+   let dBookings = [...dayBookings]
 
-      const row = document.getElementById(rowId)
-      const cell = document.getElementById(startCellId)
-      const startIndex = Array.from(row.children).indexOf(cell)
-      const cells = Array.from(row.children).slice(startIndex, (startIndex + course.duration))
+   const arrange = (startIndex = 0) => {
 
-      const c = randomColor()
-      let textColor = 'rgb(22 24 26)'
-      if (contrast(c.code) < 4.8) {
-         textColor = 'rgb(255 255 255 / .9)'
+      if (dBookings.length == 0) return
+
+      const start = dBookings[startIndex]
+      if (JSON.stringify(start) !== JSON.stringify(sorted[counter][sorted[counter].length - 1])) {
+         sorted[counter].push(start)
+         // console.log(start)
+         bookingItemUI(start, counter)
       }
 
-      cells.forEach(el => {
-         el.innerHTML += `
-         <div class="lecture" role="button" style="color: ${textColor}; background-color: ${c.color}">
-            <span class="lecture-title">${course.title}</span>
-            <span class="lecture-venue">${course.venue}</span>
-         </div>`
-      })
-   })
+      /* console.log('prev!');
+      console.log(sorted[counter][sorted[counter].length - 1]); */
+
+      if (dBookings.length == 1) {
+         return
+      }
+
+      let nextIndex = nextValidTo(startIndex)
+      dBookings.splice(startIndex, 1)
+      /* console.log('start');
+      console.log(start); */
+
+      if (nextIndex == -1) {
+         counter++
+         sorted[counter] = []
+         arrange()
+
+      } else {
+         nextIndex--
+         const next = dBookings[nextIndex]
+         sorted[counter].push(next)
+         bookingItemUI(next, counter)
+         // arrayRemove(monBookings, next)
+         /* console.log('next');
+         console.log(next);
+         console.log('---------------------'); */
+         arrange(nextIndex)
+      }
+   }
+
+   /**
+    * @param {Number} index 
+    * @returns 
+    */
+   const nextValidTo = (index) => {
+      const booking = dBookings[index]
+
+      const start1 = new Date(`2000-01-01T${booking.start}:00`)
+      let end1 = new Date(start1)
+      end1.setHours(booking.duration + end1.getHours())
+
+      for (let k = index + 1; k < dBookings.length; k++) {
+         const start2 = new Date(`2000-01-01T${dBookings[k].start}:00`)
+         if (end1 <= start2) {
+            return k
+         }
+      }
+
+      return -1
+   }
+
+   arrange()
+
+   const row = document.getElementById(`row${day}`)
+   row.style.setProperty('--rowspan', sorted.length)
+
+   return sorted
+}
+
+// wait 1 second before filling table
+
+setTimeout(() => {
+
+   // arrange data
+   for (let i = 1; i <= Object.keys(days).length; i++) {
+      let raw = data.filter(booking => booking.day == i)
+      raw.sort((a, b) => a.start.localeCompare(b.start) || (a.duration - b.duration))
+      dayData[i] = arrangeData(raw, i)
+   }
+
+   console.log(dayData);
+
 }, 1000);
 
 
@@ -112,8 +220,7 @@ const calendarBody = calendar.querySelector('.calendar-body')
 let start = new Date('2000-01-01T08:15:00')
 let end = new Date('2000-01-01T20:30:00')
 let mins = (end - start) / (1000 * 60)
-let step = 15
-let x = 0
+let step = 15 /* minutes */
 
 const days = {
    1: 'Mon',
@@ -138,25 +245,51 @@ for (let i = 1; i <= Object.keys(days).length; i++) {
 
 for (let i = 0; i < mins; i += step) {
 
-   if (start.toTimeString().slice(0, 5) == '17:15') {
-      x = 45
-   }
+   // 1-hour difference btwn time labels
 
-   if ((i + x) % 60 == 0) {
-      let s = new Date(start.getTime())
-      s.setHours((1 + s.getHours()))
-      const sTime = start.toTimeString().slice(0, 5)
-      const eTime = s.toTimeString().slice(0, 5)
-      calendarHeader.querySelector('.calendar-row')
-         .innerHTML += `<div class="calendar-cell">${sTime} - ${eTime}</div>`
-      calendarBody.querySelectorAll('.calendar-row').forEach(el => {
-         let id = el.getAttribute('id').slice(3)   /* row number */
-         id += sTime.slice(0, 2) + sTime.slice(3)  /* time without colon : */
-         el.innerHTML += `<div class="calendar-cell calendar-body-cell" id="cell${id}"></div>`
-      })
-   }
+   let sTime = start.toTimeString().slice(0, 5)
+   let sTimeLabel = (i % 60 == 0) ? sTime : ''
+
+   calendarHeader.querySelector('.calendar-row')
+      .innerHTML += `<div class="calendar-cell"><span class="time-indicator">${sTimeLabel}</span></div>`
+      
+   calendarBody.querySelectorAll('.calendar-row').forEach(row => {
+      let rowId = row.getAttribute('id').slice(3)   /* row number */
+      rowId += removeColon(sTime)  /* time without colon : */
+      row.innerHTML += `<div class="calendar-cell calendar-body-cell" id="cell${rowId}"></div>`
+   })
 
    start.setMinutes((start.getMinutes() + step))
+}
+
+/**
+ * @param {String} s time string H:M
+ */
+function removeColon(s) {
+   return s.slice(0, 2) + s.slice(3)
+}
+
+/**
+ * @param {Object} course 
+ * @param {Number} top 
+ */
+function bookingItemUI(course, top = 0) {
+   const startCellId = `cell${course.day}${removeColon(course.start)}`
+   const startCell = document.getElementById(startCellId)
+   const colspan = 4 * course.duration
+
+   const c = randomColor()
+   let textColor = 'rgb(22 24 26)'
+   if (contrast(c.code) < 4.8) {
+      textColor = 'rgb(255 255 255 / .9)'
+   }
+
+   startCell.innerHTML += `<div class="calendar-booking-item" style="--top: ${top}; --colspan: ${colspan}">
+      <div class="lecture" role="button" style="color: ${textColor}; background-color: ${c.color}">
+         <span class="lecture-title">${course.title}</span>
+         <span class="lecture-venue">${course.venue} - ${course.duration} hrs</span>
+      </div>
+   </div>`
 }
 
 // extras
